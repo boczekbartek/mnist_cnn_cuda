80c7325444677706f176296a0d2e3381761ef0ce
Bartłomiej Boczek <bartek1boczek@gmail.com>
2018-11-26 12:50:38 +0100

    Reduced hidden layer size of basic_nn to fit simple_cnn.

--- mnist_py/models/basic_nn.py
+++ mnist_py/models/basic_nn.py
@@ -8,7 +8,7 @@ def get_basic_nn_model():
     model.add(Dense(20, input_shape=(28, 28, 1)))
     model.add(Flatten())
     model.add(Dropout(0.2))
-    model.add(Dense(256))
+    model.add(Dense(64))
     model.add(Dropout(0.2))
     model.add(Activation('sigmoid'))
     model.add(Dense(10))
